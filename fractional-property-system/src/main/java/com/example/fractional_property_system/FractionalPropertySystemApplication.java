cc03008ea8eeb274b0d9a95dae7287cbc3363907
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2025-01-08 10:26:37 +0100

    init project

---
+++ fractional-property-system/src/main/java/com/example/fractional_property_system/FractionalPropertySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fractional_property_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FractionalPropertySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FractionalPropertySystemApplication.class, args);
+	}
+
+}
---
+++ fractional-property-system/src/test/java/com/example/fractional_property_system/FractionalPropertySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fractional_property_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FractionalPropertySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
